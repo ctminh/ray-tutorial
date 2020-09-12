9835094b14bdb7fc103eceb0ee6bf5136a21eb2e
Thanh Chung <minh.thanh.chung@nm.ifi.lmu.de>
2020-09-12 18:11:40 +0200

    update pi example with ray

--- tutorial/pi_ray/pi_calc_with_ray.py
+++ tutorial/pi_ray/pi_calc_with_ray.py
@@ -34,5 +34,5 @@ def try_it(n, trials):
     print(fmt.format(duration, approx_pi, stdev, error))   # str_large_n imported above.
     return trials, n, duration, approx_pi, stdev, error
 
-# data_ns = [try_it(n, trials) for n in Ns]
+data_ns = [try_it(n, trials) for n in Ns]
 # data_trials = [try_it(maxN, trials) for trials in range(5,20,2)]
