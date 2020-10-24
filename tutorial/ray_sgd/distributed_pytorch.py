c4563c86c6e24a486d56baa2b7d80de1b4d36c02
Thanh Chung <minh.thanh.chung@nm.ifi.lmu.de>
2020-10-24 14:49:59 +0200

    try distributed pytorch

--- tutorial/ray_sgd/distributed_pytorch.py
+++ tutorial/ray_sgd/distributed_pytorch.py
@@ -47,7 +47,7 @@ ray.init()
 trainer = TorchTrainer(
     training_operator_cls=MyTrainingOperator,
     scheduler_step_freq="epoch",  # if scheduler is used
-    config={"lr": 0.001, "batch_size": 64},
+    config={"lr": 0.001, "batch_size": 64}
     # num_workers=100,
     # use_gpu=True
     )
